82c3f511cb0b8b10bacf2906fe73bcfb915080c8
Amanpydv <amanpydv@gmail.com>
2024-11-25 10:59:24 -0500

    Transformation - Business Rule
    -Excluded orders with a quantity of 0 or less.
    -Marked orders with total values exceeding $1000 as "High-Value Orders" in a new column order_type.
    - Add a column customer_tenure representing the number of days since the
    customer's signup date.

--- pipeline.py
+++ pipeline.py
@@ -56,18 +56,25 @@ for file in files:
             sales_df['order_id'] = pd.to_numeric(sales_df['order_id'], errors='coerce')
             sales_df['customer_id'] = pd.to_numeric(sales_df['customer_id'], errors='coerce')
             sales_df_clean = sales_df.dropna(subset=['order_id', 'customer_id']) #Remove any invalid customer id and order id
-            sales_df['total_value'] = sales_df['quantity'] * sales_df['price'] #Calculating total value of each order
             #print("\n Successfully Formatted Order_Date and Checked for any invalid customer and order id: \n\n",sales_df)
-            print("\nTables after data enrichment \n\n",sales_df)
+            sales_df['total_value'] = sales_df['quantity'] * sales_df['price'] #Calculating total value of each order
+            #print("\nTables after data enrichment \n\n",sales_df)
+            sales_df = sales_df[sales_df['quantity'] > 0].copy() #excluding quantity with 0 or less
+            sales_df['order_type'] = np.where(sales_df['total_value'] > 1000, 'High-Value Order', 'Regular Order') #Marking any total_value above 1000 as high order and rest as regular order
+            print("\n Sales Data Table after Business rule\n\n",sales_df)
         if file== 'customer_data.csv':
             customer_df = df
             customer_df['signup_date'] = pd.to_datetime(customer_df['signup_date'], errors='coerce').dt.date #converting signup_date in customer table to datetime format
             customer_df['customer_id'] = pd.to_numeric(customer_df['customer_id'], errors='coerce')
             customer_df_clean = customer_df.dropna(subset=['customer_id']) #Remove any invalid customer id
             #print("\n Successfully Formatted Signup_Date and Checked for any invalid customer id: \n\n",customer_df)
+            customer_df['signup_date'] = pd.to_datetime(customer_df['signup_date'])
+            current_date = pd.to_datetime(datetime.now().date())
+            customer_df['customer_tenure'] = (current_date - customer_df['signup_date']).dt.days
+            print("\nCustomer Data after Business rule\n\n",customer_df)
 
 #Join the sales data with customer data on customer_id to enrich the sales table with customer_name and email.            
 if sales_df is not None and customer_df is not None:
     enriched_sales_df = pd.merge(sales_df, customer_df[['customer_id', 'customer_name', 'email']], 
                                  on='customer_id', how='left')
-    print("\nEnriched Sales_df\n\n",enriched_sales_df)
+    #print("\nEnriched Sales_df\n\n",enriched_sales_df)
